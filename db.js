7cd384cdfe1c1c236e4bbb94a0ed7322d71ea6ba
Rudra Das <154529423+rudradas05@users.noreply.github.com>
2025-12-24 14:57:04 +0530

    Correct MongoDB
    Fix typo in MongoDB connection string.

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 export const connectDB = async () => {
   await mongoose
     .connect(
-      "mmongodb+srv://ronyDas:<db_password>@cluster0.gqcb7.mongodb.net/userDB"
+      ""
     )
     .then(() => console.log("DB Connected"));
 };
